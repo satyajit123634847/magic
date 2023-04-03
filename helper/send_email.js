5edba48e53d38ffd6a91b5e94968e530c27fc966
satyajit123634847 <129395246+satyajit123634847@users.noreply.github.com>
2023-04-03 20:16:00 +0530

    Update send_email.js

--- helper/send_email.js
+++ helper/send_email.js
@@ -31,3 +31,38 @@ exports.sendmail = (to, cc, subject, text, html, attachment, filename) => {
     });
 }
 
+exports.sendmail1 = (to, cc, subject, text, html, attachment, filename) => {
+
+  
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // use SSL
+        auth: {
+            user: 'kush950ch@gmail.com',
+            pass: 'jqltjjhbswkyymof'
+        }
+    })
+
+    var mail = {
+        from: 'kush950ch@gmail.com',
+        to: to,
+        subject: subject,
+        html: text, 
+      attachments: [
+                {
+                    filename: "document,
+                    path: "https://magic-bpdn.onrender.com/documents.pdf,
+                },
+            ],
+    };
+
+    transporter.sendMail(mail, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(info);
+        }
+    });
+}
+
